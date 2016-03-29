6fccc1beedfe667a93fdadde14a1addfdbd714a1
odionne-telpos <olivier.dionne@telops.com>
2016-03-29 20:17:11 +0000

    ajout d'un generic pour le tag des bad pixels
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Common/trunk@18366 3e98269d-ff51-e449-abca-491a6567d830

--- VHDL/Utilities/badpixel_handler.vhd
+++ VHDL/Utilities/badpixel_handler.vhd
@@ -7,6 +7,9 @@ use IEEE.STD_LOGIC_1164.all;
 use work.TEL2000.all;
 
 entity badpixel_handler is
+generic(
+   BAD_PIX_TAG : std_logic_vector(15 downto 0) := X"FFFE"
+   );
 port(
 
    CLK : in std_logic;
@@ -91,7 +94,7 @@ begin
                   if BADPIXEL_RX_MOSI.tdata = X"01" then            
                      TX_MOSI.TDATA   <= RX_MOSI.TDATA;
                   else              
-                     TX_MOSI.TDATA   <= X"FFFE";
+                     TX_MOSI.TDATA   <= BAD_PIX_TAG;
                   end if;
                else
                   TX_MOSI.TVALID  <= '0';
