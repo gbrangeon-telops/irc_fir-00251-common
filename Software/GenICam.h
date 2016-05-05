a2d87c0dfb470576d0a51eebee89ea140f2f301d
dalain-telpos <denis.alain@telops.com>
2016-05-05 14:46:48 +0000

    XML 11.4.1
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Common/trunk@18684 3e98269d-ff51-e449-abca-491a6567d830

--- Software/GenICam.h
+++ Software/GenICam.h
@@ -127,12 +127,12 @@ typedef struct gcRegister gcRegister_t;
 
 /* AUTO-CODE BEGIN */
 // Auto-generated GeniCam library.
-// Generated from XML camera definition file version 11.4.0
+// Generated from XML camera definition file version 11.4.1
 // using generateGenICamCommonCLib.m Matlab script.
 
 #define GC_XMLMAJORVERSION    11
 #define GC_XMLMINORVERSION    4
-#define GC_XMLSUBMINORVERSION 0
+#define GC_XMLSUBMINORVERSION 1
 
 // Enumerations values and data types
 ////////////////////////////////////////////////////////////////////////////////
