df173f3365a126468d8a794b47a636e9a15c876c
odionne-telpos <olivier.dionne@telops.com>
2019-05-16 21:17:26 +0000

    XML 12.5.1 (remplace r23551)
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Common/trunk@23557 3e98269d-ff51-e449-abca-491a6567d830

--- Software/GenICam.c
+++ Software/GenICam.c
@@ -215,7 +215,7 @@ gcRegister_t gcRegsDef[GC_REG_COUNT] = {
    /* DevicePowerOnCycles */                       {0x0000EAA0, NULL, NULL, 4, 4, GCRO_Processing_FPGA, REG_UNLOCKED|REG_INT|REG_RO|REG_NOTSHARED|REG_BIGENDIAN},
    /* DeviceCoolerPowerOnCycles */                 {0x0000EAA4, NULL, NULL, 4, 4, GCRO_Processing_FPGA, REG_UNLOCKED|REG_INT|REG_RO|REG_NOTSHARED|REG_BIGENDIAN},
    /* TDCFlags */                                  {0x0000EAA8, NULL, NULL, 4, 4, GCRO_Processing_FPGA, REG_UNLOCKED|REG_INT|REG_RO|REG_NOTSHARED|REG_BIGENDIAN},
-   /* TDCStatus */                                 {0x0000EAAC, NULL, NULL, 4, 4, GCRO_Processing_FPGA, REG_UNLOCKED|REG_INT|REG_RO|REG_SHARED|REG_BIGENDIAN},
+   /* TDCStatus */                                 {0x0000EAAC, NULL, NULL, 4, 4, GCRO_Processing_FPGA, REG_UNLOCKED|REG_INT|REG_RO|REG_NOTSHARED|REG_BIGENDIAN},
    /* AvailabilityFlags */                         {0x0000EAB0, NULL, NULL, 4, 4, GCRO_Processing_FPGA, REG_UNLOCKED|REG_INT|REG_RO|REG_NOTSHARED|REG_BIGENDIAN},
    /* AcquisitionFrameRateMax */                   {0x0000EAB4, NULL, NULL, 4, 4, GCRO_Processing_FPGA, REG_UNLOCKED|REG_FLOAT|REG_RO|REG_NOTSHARED|REG_BIGENDIAN},
    /* ExposureTimeMin */                           {0x0000EAB8, NULL, NULL, 4, 4, GCRO_Processing_FPGA, REG_UNLOCKED|REG_FLOAT|REG_RO|REG_NOTSHARED|REG_BIGENDIAN},
