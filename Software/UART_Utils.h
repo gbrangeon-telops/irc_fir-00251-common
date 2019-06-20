b81b88af33c7b4585b3f56d1f26b605529141812
pcouture-telpos <philippe.couture@telops.com>
2019-06-20 15:55:54 +0000

    Bug Fix de la communication avec la Filter Wheel (#11300)
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Common/trunk@23776 3e98269d-ff51-e449-abca-491a6567d830

--- Software/UART_Utils.h
+++ Software/UART_Utils.h
@@ -13,28 +13,28 @@
  *
  * (c) Copyright 2014 Telops Inc.
  */
-//#ifndef UART_UTILS_H
-//#define UART_UTILS_H
-//
-//#include <stdint.h>
-////#include "xuartns550.h"
-//#include "xintc.h"
-//#include "IRC_Status.h"
-//
-//
-////IRC_Status_t UART_Init(XUartNs550 *uart,
-////      uint16_t uartDeviceId,
-////      XIntc *intc,
-////      uint16_t uartIntrId,
-////      XUartNs550_Handler handler,
-////      void *callbackRef);
-////
-////IRC_Status_t UART_Config(XUartNs550 *uart,
-////      uint32_t baudRate,
-////      uint8_t dataBits,
-////      char parity,
-////      uint8_t stopBits);
-////
-////IRC_Status_t UART_ResetRxFifo(XUartNs550 *uart);
-//
-//#endif // UART_UTILS_H
+#ifndef UART_UTILS_H
+#define UART_UTILS_H   
+
+#include <stdint.h>
+#include "xuartns550.h"
+#include "xintc.h"
+#include "IRC_Status.h"
+
+
+IRC_Status_t UART_Init(XUartNs550 *uart,
+      uint16_t uartDeviceId,
+      XIntc *intc,
+      uint16_t uartIntrId,
+      XUartNs550_Handler handler,
+      void *callbackRef);
+
+IRC_Status_t UART_Config(XUartNs550 *uart,
+      uint32_t baudRate,
+      uint8_t dataBits,
+      char parity,
+      uint8_t stopBits);
+
+IRC_Status_t UART_ResetRxFifo(XUartNs550 *uart);
+
+#endif // UART_UTILS_H
