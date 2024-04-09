35aa6c11f696a1fa75eb825f32482d577a626e00
alacasse-telpos <alexis.lacasse@telops.com>
2024-04-09 16:39:43 +0000

    Refactor axis16_combine_axis64.vhd, the AXIS Combine IP doesn't assert RX TREADY until all RX TVALID are asserted. Some module doesn't rise their TVALID until TREADY is asserted
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Common/trunk@30183 3e98269d-ff51-e449-abca-491a6567d830

--- VHDL/Utilities/axis16_combine_axis64.vhd
+++ VHDL/Utilities/axis16_combine_axis64.vhd
@@ -11,6 +11,7 @@
 
 library IEEE;
 use IEEE.std_logic_1164.all;   
+use IEEE.std_logic_misc.all;   
 use ieee.numeric_std.all;
 use work.tel2000.all;
 
@@ -29,104 +30,24 @@ entity axis16_combine_axis64 is
       );
 end axis16_combine_axis64;
 
-architecture RTL of axis16_combine_axis64 is 
-   
-   signal s_axis_tvalid : std_logic_vector(3 downto 0);
-   signal s_axis_tready : std_logic_vector(3 downto 0);
-   signal s_axis_tdata : std_logic_vector(63 downto 0);
-   signal s_axis_tstrb : std_logic_vector(7 downto 0);
-   signal s_axis_tkeep : std_logic_vector(7 downto 0);
-   signal s_axis_tlast : std_logic_vector(3 downto 0);
-   signal s_axis_tid : std_logic_vector(3 downto 0);
-   signal s_axis_tdest : std_logic_vector(11 downto 0);
-   signal s_axis_tuser : std_logic_vector(15 downto 0);
-   signal s0_cmd_err : std_logic_vector(2 downto 0);
-   signal s1_cmd_err : std_logic_vector(2 downto 0);
-   signal s2_cmd_err : std_logic_vector(2 downto 0);
-   signal s3_cmd_err : std_logic_vector(2 downto 0);
-   
-   COMPONENT ip_axis16_combine_axis64
-      PORT (
-         aclk : IN STD_LOGIC;
-         aresetn : IN STD_LOGIC;
-         s_axis_tvalid : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-         s_axis_tready : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
-         s_axis_tdata : IN STD_LOGIC_VECTOR(63 DOWNTO 0);
-         s_axis_tstrb : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-         s_axis_tkeep : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-         s_axis_tlast : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-         s_axis_tid : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-         s_axis_tdest : IN STD_LOGIC_VECTOR(11 DOWNTO 0);
-         s_axis_tuser : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-         m_axis_tvalid : OUT STD_LOGIC;
-         m_axis_tready : IN STD_LOGIC;
-         m_axis_tdata : OUT STD_LOGIC_VECTOR(63 DOWNTO 0);
-         m_axis_tstrb : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
-         m_axis_tkeep : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
-         m_axis_tlast : OUT STD_LOGIC;
-         m_axis_tid : OUT STD_LOGIC_VECTOR(0 DOWNTO 0);
-         m_axis_tdest : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
-         m_axis_tuser : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
-         s_cmd_err : OUT STD_LOGIC_VECTOR(11 DOWNTO 0)
-      );
-   END COMPONENT;
-   
-   
-   
+architecture RTL_ALT of axis16_combine_axis64 is
+	signal TVALID : std_logic_vector(RX_MOSI_ARY'RANGE);
+	signal TLAST  : std_logic_vector(RX_MOSI_ARY'RANGE);
 begin
-   
-   RX_MISO_ARY(0).tready <= s_axis_tready(0);
-   RX_MISO_ARY(1).tready <= s_axis_tready(1);
-   RX_MISO_ARY(2).tready <= s_axis_tready(2);
-   RX_MISO_ARY(3).tready <= s_axis_tready(3);
-   
-   s_axis_tvalid  <= RX_MOSI_ARY(3).tvalid & RX_MOSI_ARY(2).tvalid & RX_MOSI_ARY(1).tvalid & RX_MOSI_ARY(0).tvalid;
-   s_axis_tdata   <= RX_MOSI_ARY(3).tdata  & RX_MOSI_ARY(2).tdata  & RX_MOSI_ARY(1).tdata  & RX_MOSI_ARY(0).tdata;
-   s_axis_tstrb   <= RX_MOSI_ARY(3).tstrb  & RX_MOSI_ARY(2).tstrb  & RX_MOSI_ARY(1).tstrb  & RX_MOSI_ARY(0).tstrb;
-   s_axis_tkeep   <= RX_MOSI_ARY(3).tkeep  & RX_MOSI_ARY(2).tkeep  & RX_MOSI_ARY(1).tkeep  & RX_MOSI_ARY(0).tkeep;
-   s_axis_tlast   <= RX_MOSI_ARY(3).tlast  & RX_MOSI_ARY(2).tlast  & RX_MOSI_ARY(1).tlast  & RX_MOSI_ARY(0).tlast;
-   s_axis_tid     <= RX_MOSI_ARY(3).tid    & RX_MOSI_ARY(2).tid    & RX_MOSI_ARY(1).tid    & RX_MOSI_ARY(0).tid;
-   s_axis_tdest   <= RX_MOSI_ARY(3).tdest  & RX_MOSI_ARY(2).tdest  & RX_MOSI_ARY(1).tdest  & RX_MOSI_ARY(0).tdest;
-   s_axis_tuser   <= RX_MOSI_ARY(3).tuser  & RX_MOSI_ARY(2).tuser  & RX_MOSI_ARY(1).tuser  & RX_MOSI_ARY(0).tuser;
-   
-   ERR <= s0_cmd_err or s1_cmd_err or s2_cmd_err or s3_cmd_err;
-   
-   
-   U1 : ip_axis16_combine_axis64
-   port map (
-      aclk => CLK,
-      aresetn => ARESETN,
-      
-      s_axis_tvalid => s_axis_tvalid,
-      s_axis_tready => s_axis_tready,
-      s_axis_tdata => s_axis_tdata,
-      s_axis_tstrb => s_axis_tstrb,
-      s_axis_tkeep => s_axis_tkeep,
-      s_axis_tlast => s_axis_tlast,
-      s_axis_tid => s_axis_tid,
-      s_axis_tdest => s_axis_tdest,
-      s_axis_tuser => s_axis_tuser,
-      
-      m_axis_tvalid => TX_MOSI.tvalid,
-      m_axis_tready => TX_MISO.tready,
-      m_axis_tdata => TX_MOSI.tdata,
-      m_axis_tstrb => TX_MOSI.tstrb,
-      m_axis_tkeep => TX_MOSI.tkeep,
-      m_axis_tlast => TX_MOSI.tlast,
-      m_axis_tid => TX_MOSI.tid,
-      m_axis_tdest => TX_MOSI.tdest,
-      m_axis_tuser => TX_MOSI.tuser,
-      
-      -- When S00 is primary slave: s_cmd_err(11:9) => errors and others => gnd
-      -- When S01 is primary slave: s_cmd_err(8:6) => errors and others => gnd
-      -- When S03 is primary slave: s_cmd_err(2:0) => errors and others => gnd
-      s_cmd_err(11 downto 9) => s0_cmd_err,
-      s_cmd_err(8 downto 6) => s1_cmd_err,
-      s_cmd_err(5 downto 3) => s2_cmd_err,
-      s_cmd_err(2 downto 0) => s3_cmd_err
-      );
-   
-   
-end RTL;
-
-
+	RX_MISO_ARY <= (others => TX_MISO);
+	
+	G0 : for i in RX_MOSI_ARY'RANGE generate
+      TVALID(i) <= RX_MOSI_ARY(i).TVALID;
+      TX_MOSI.TDATA((i+1)*RX_MOSI_ARY(i).TDATA'LENGTH-1 downto i*RX_MOSI_ARY(i).TDATA'LENGTH) <= RX_MOSI_ARY(i).TDATA;
+      TX_MOSI.TSTRB((i+1)*RX_MOSI_ARY(i).TSTRB'LENGTH-1 downto i*RX_MOSI_ARY(i).TSTRB'LENGTH) <= RX_MOSI_ARY(i).TSTRB;
+      TX_MOSI.TKEEP((i+1)*RX_MOSI_ARY(i).TKEEP'LENGTH-1 downto i*RX_MOSI_ARY(i).TKEEP'LENGTH) <= RX_MOSI_ARY(i).TKEEP;
+      TLAST(i) <= RX_MOSI_ARY(i).TLAST;
+      TX_MOSI.TUSER((i+1)*RX_MOSI_ARY(i).TUSER'LENGTH-1 downto i*RX_MOSI_ARY(i).TUSER'LENGTH) <= RX_MOSI_ARY(i).TUSER;
+    end generate;
+	TX_MOSI.TVALID <= and_reduce(TVALID);
+	TX_MOSI.TLAST <= or_reduce(TLAST);
+	TX_MOSI.TID <= RX_MOSI_ARY(0).TID;
+    TX_MOSI.TDEST <= RX_MOSI_ARY(0).TDEST;
+	
+	ERR <= (others => '0');
+end RTL_ALT;
